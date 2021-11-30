917c78b1b90deb7b2d746374bde6d6c0d50a8eb5
CarlosPercca <cperccaa@unjbg.edu.pe>
2021-11-30 08:34:27 -0500

    implementacion de la clase persona

---
+++ persona.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+class Persona{
+    protected:
+    string DNI;
+    string nombres;
+    int edad;
+    public:
+    Persona(string, string, int);
+    void mostrarPersona();
+};
+Persona :: Persona(string d , string n , int e){
+    DNI= d;
+    nombres = n;
+    edad = e;
+}
+void Persona :: mostrarPersona(){
+    cout<<nombres<< " indentificado con DNI N : "<<DNI;
+    cout<<" tiene "<<edad<<" anios de edad."<<endl;
+}
+
+int main(){
+    Persona Carlos("40786723", "Carlos Arias", 24);
+    Carlos.mostrarPersona();
+
+    return 0;
+}
