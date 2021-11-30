54938b2d61278d88e7f3e331821769e71a9184b9
CarlosPercca <cperccaa@unjbg.edu.pe>
2021-11-30 08:58:59 -0500

    implementacion de la clase Docente

--- persona.cpp
+++ persona.cpp
@@ -10,6 +10,15 @@ class Persona{
     Persona(string, string, int);
     void mostrarPersona();
 };
+class Docente : public Persona{
+    private:
+        float sueldo;
+        string categoria;
+    public:
+        Docente(string , string , int , float , string);
+        void mostrarDocente();
+};
+
 Persona :: Persona(string d , string n , int e){
     DNI= d;
     nombres = n;
@@ -20,9 +29,23 @@ void Persona :: mostrarPersona(){
     cout<<" tiene "<<edad<<" anios de edad."<<endl;
 }
 
+Docente :: Docente (string d, string n, int e , float s , string c):Persona(d,n,e){
+    sueldo=s;
+    categoria=c;
+}
+void Docente :: mostrarDocente(){
+mostrarPersona();
+    cout<<"\t Tiene un salario de S/ "<<sueldo<<" y es docente"<<categoria;
+    cout<<endl;
+}
+
 int main(){
     Persona Carlos("40786723", "Carlos Arias", 24);
     Carlos.mostrarPersona();
+    cout<<"-----------------------"<<endl;
+    Docente Maria("40845452","Maria Zevallos",30,2000,"contratada");
+    Maria.mostrarPersona();
+    Maria.mostrarDocente();
 
     return 0;
 }
